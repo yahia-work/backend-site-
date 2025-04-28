868ce623ef6260e07de280adbb844359a53fa5e1
codingdev3105 <codingdev3105@gmail.com>
2025-04-28 22:12:36 +0100

    Initial commit: backend setup with Express and Vercel

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Vercel from Express!');
+});
+
+// Export pour Vercel
+module.exports = app;
