fd9aa546ba8dabf9bcfc22cfbdbd143c4df93dff
codingdev3105 <codingdev3105@gmail.com>
2025-04-29 01:11:18 +0100

    deleted json 7

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const sheetRoutes = require('./Routes/SheetRoutes');
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Exemple de route
-app.get('/users', (req, res) => {
-  res.json({ message: 'Bienvenue sur l\'API des utilisateurs' });
+
+app.use('/read', sheetRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Hello yahia');
+});
+
+app.get('/verceltest', (req, res) => {
+    res.send('Hello Vercel from Express!');
 });
 
 module.exports = app; // Exporter l'application pour Vercel
