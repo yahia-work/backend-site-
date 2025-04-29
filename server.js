610e35d5e51b2d1003475fc51cbb101954b34cf3
codingdev3105 <codingdev3105@gmail.com>
2025-04-29 01:09:09 +0100

    deleted json 6

--- server.js
+++ server.js
@@ -16,11 +16,5 @@ app.get('/users', (req, res) => {
   res.json({ message: 'Bienvenue sur l\'API des utilisateurs' });
 });
 
-// Si on est en mode serverless sur Vercel
-if (process.env.VERCEL) {
-  module.exports = app; // Exporter l'application pour Vercel
-} else {
-  app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`);
-  });
-}
+module.exports = app; // Exporter l'application pour Vercel
+
